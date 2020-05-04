1e63fedb5fb709d12896fc4cdee9405e9b47ae5f
Daniel Juanda <danieljuanda276@gmail.com>
2020-05-04 07:52:50 +0100

    la

--- src/layout/static/Success.jsx
+++ src/layout/static/Success.jsx
@@ -9,7 +9,7 @@ class Success extends Component {
         <div className="status-page">
           <div className="box">
             <h3 style={{color: 'green'}}>Attendance Successfully Taken</h3>
-            <button className="btn btn-outline-dark btn-block mt-3"><a href="http://localhost">Back to Device</a></button>
+            <button className="btn btn-outline-dark btn-block mt-3"><a href="http://localhost:8080">Back to Device</a></button>
           </div>
         </div>
     );
